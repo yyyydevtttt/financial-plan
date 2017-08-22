7c0b1b436b576007a0aea4c231bb525908a004f9
yoshidome.taiki <yyyydevtttt@gmail.com>
2017-08-22 17:34:05 +0900

    family の実装　すこし

--- src/app/family/family.module.ts
+++ src/app/family/family.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 
 import { FamilyRoutingModule } from './family-routing.module';
 import { FamilyComponent } from "app/family/family.component";
@@ -7,7 +8,8 @@ import { FamilyComponent } from "app/family/family.component";
 @NgModule({
   imports: [
     CommonModule,
-    FamilyRoutingModule
+    FamilyRoutingModule,
+    FormsModule
   ],
   declarations: [FamilyComponent]
 })
